cd2bbefbfb78e44e508d5a3491be22fb50347a84
Konrad Kleeberger <git@konrad.online>
2018-03-30 21:37:57 +0200

    refactoring: replaced list with normal arrays

--- app/src/main/java/com/example/example6/MainActivity.java
+++ app/src/main/java/com/example/example6/MainActivity.java
@@ -73,7 +73,7 @@ public class MainActivity extends Activity implements OnClickListener {
 
     private Integer totalParticles;
 
-    private List<ArrayList<Integer>> Particles;
+    private List<int[]> Particles;
 
     private Set<Integer> collidedParticles;
 
@@ -81,9 +81,8 @@ public class MainActivity extends Activity implements OnClickListener {
 
     private  int radiusParticles = 5;
 
-
-
     private List<ShapeDrawable> walls;
+    private List<int[]> wallsBounds;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -91,10 +90,9 @@ public class MainActivity extends Activity implements OnClickListener {
 
         // set the buttons
         up = (Button) findViewById(R.id.button1);
-        left = (Button) findViewById(R.id.button2);
         right = (Button) findViewById(R.id.button3);
         down = (Button) findViewById(R.id.button4);
-
+        left = (Button) findViewById(R.id.button2);
         // set the text view
         textView = (TextView) findViewById(R.id.textView1);
 
@@ -113,149 +111,49 @@ public class MainActivity extends Activity implements OnClickListener {
 
 
         walls = new ArrayList<>();
+        wallsBounds = new ArrayList<int[]>();
         this.propabilityRoom = new HashMap<Integer, Integer>();
 
         this.collidedParticles = new HashSet<Integer>();
-        /*
-        ShapeDrawable d = new ShapeDrawable(new RectShape());
-        d.setBounds(width/2-200, height/2-90, width/2+200, height/2-80);
-        ShapeDrawable d2 = new ShapeDrawable(new RectShape());
-        d2.setBounds(width/2-200, height/2+60, width/2+200, height/2+70);
-        ShapeDrawable d3 = new ShapeDrawable(new RectShape());
-        d3.setBounds(width/2+200, height/2-90, width/2+210, height/2+70);
-        walls.add(d);
-        walls.add(d2);
-        walls.add(d3);
-        */
 
-        ShapeDrawable h1 = new ShapeDrawable(new RectShape());
-        ShapeDrawable h2 = new ShapeDrawable(new RectShape());
-        ShapeDrawable h3 = new ShapeDrawable(new RectShape());
-        ShapeDrawable h4 = new ShapeDrawable(new RectShape());
-        ShapeDrawable h5 = new ShapeDrawable(new RectShape());
-        ShapeDrawable h6 = new ShapeDrawable(new RectShape());
-        ShapeDrawable h7 = new ShapeDrawable(new RectShape());
-        ShapeDrawable h8 = new ShapeDrawable(new RectShape());
-        ShapeDrawable h9 = new ShapeDrawable(new RectShape());
-        ShapeDrawable h10 = new ShapeDrawable(new RectShape());
-        ShapeDrawable h11 = new ShapeDrawable(new RectShape());
-        ShapeDrawable h12 = new ShapeDrawable(new RectShape());
-        ShapeDrawable h13 = new ShapeDrawable(new RectShape());
-        ShapeDrawable h14 = new ShapeDrawable(new RectShape());
-        ShapeDrawable h15 = new ShapeDrawable(new RectShape());
-        ShapeDrawable h16 = new ShapeDrawable(new RectShape());
-        ShapeDrawable h17 = new ShapeDrawable(new RectShape());
-        ShapeDrawable h18 = new ShapeDrawable(new RectShape());
-        ShapeDrawable h19 = new ShapeDrawable(new RectShape());
-        ShapeDrawable h20 = new ShapeDrawable(new RectShape());
-        ShapeDrawable h21 = new ShapeDrawable(new RectShape());
+        wallsBounds.add(new int[]{0,0,2600,5});
+        wallsBounds.add(new int[]{1950,500,2600,505});
+        wallsBounds.add(new int[]{1830,1230,2170,1235});
+        wallsBounds.add(new int[]{1600,920,1830,925});
+        wallsBounds.add(new int[]{0,920,920,925});
+        wallsBounds.add(new int[]{0,1440,2600,1445});
+        wallsBounds.add(new int[]{440,690,1010,695});
+        wallsBounds.add(new int[]{1240,690,1820,695});
+        wallsBounds.add(new int[]{2170,850,2600,855});
+        wallsBounds.add(new int[]{2170,1080,2600,1085});
+        wallsBounds.add(new int[]{0,130,440,135});
+        wallsBounds.add(new int[]{1380,130,1820,135});
+        wallsBounds.add(new int[]{580,270,1010,275});
+        wallsBounds.add(new int[]{1010,270,1240,275});
+        wallsBounds.add(new int[]{1240,270,1380,275});
+        wallsBounds.add(new int[]{440,320,580,325});
+        wallsBounds.add(new int[]{0,520,440,525});
+        wallsBounds.add(new int[]{0,0,5,1440});
+        wallsBounds.add(new int[]{2600,0,2605,1440});
+        wallsBounds.add(new int[]{2170,500,2175,850});
+        wallsBounds.add(new int[]{2170,1080,2175,1440});
+        wallsBounds.add(new int[]{1820,130,1825,695});
+        wallsBounds.add(new int[]{1830,920,1835,1440});
+        wallsBounds.add(new int[]{1600,920,1605,1440});
+        wallsBounds.add(new int[]{1260,920,1265,1440});
+        wallsBounds.add(new int[]{920,920,925,1440});
+        wallsBounds.add(new int[]{440,130,445,690});
+        wallsBounds.add(new int[]{580,270,585,690});
+        wallsBounds.add(new int[]{1010,270,1015,690});
+        wallsBounds.add(new int[]{1240,270,1245,690});
+        wallsBounds.add(new int[]{1380,130,1385,690});
+        wallsBounds.add(new int[]{1950,0,1955,500});
 
-        ShapeDrawable v1 = new ShapeDrawable(new RectShape());
-        ShapeDrawable v2 = new ShapeDrawable(new RectShape());
-        ShapeDrawable v3 = new ShapeDrawable(new RectShape());
-        ShapeDrawable v4 = new ShapeDrawable(new RectShape());
-        ShapeDrawable v5 = new ShapeDrawable(new RectShape());
-        ShapeDrawable v6 = new ShapeDrawable(new RectShape());
-        ShapeDrawable v7 = new ShapeDrawable(new RectShape());
-        ShapeDrawable v8 = new ShapeDrawable(new RectShape());
-        ShapeDrawable v9 = new ShapeDrawable(new RectShape());
-        ShapeDrawable v10 = new ShapeDrawable(new RectShape());
-        ShapeDrawable v11 = new ShapeDrawable(new RectShape());
-        ShapeDrawable v12 = new ShapeDrawable(new RectShape());
-        ShapeDrawable v13 = new ShapeDrawable(new RectShape());
-        ShapeDrawable v14 = new ShapeDrawable(new RectShape());
-        ShapeDrawable v15 = new ShapeDrawable(new RectShape());
-        ShapeDrawable v16 = new ShapeDrawable(new RectShape());
-        ShapeDrawable v17 = new ShapeDrawable(new RectShape());
-        ShapeDrawable v18 = new ShapeDrawable(new RectShape());
-        ShapeDrawable v19 = new ShapeDrawable(new RectShape());
-        ShapeDrawable v20 = new ShapeDrawable(new RectShape());
-
-
-
-
-        h1.setBounds(0,0,2600,5);
-        h2.setBounds(1950,500,2600,505);
-        h3.setBounds(1830,1230,2170,1235);
-        h4.setBounds(1600,920,1830,925);
-        h5.setBounds(1260,920,1600,925);
-        h6.setBounds(920,920,1260,925);
-        h7.setBounds(0,920,920,925);
-        h8.setBounds(0,1440,2600,1445);
-        h9.setBounds(0,690,440,695);
-        h10.setBounds(440,690,1010,695);
-        h11.setBounds(1010,690,1240,695);
-        h12.setBounds(1240,690,1820,695);
-        h13.setBounds(2170,850,2600,855);
-        h14.setBounds(2170,1080,2600,1085);
-        h15.setBounds(0,130,440,135);
-        h16.setBounds(1380,130,1820,135);
-        h17.setBounds(580,270,1010,275);
-        h18.setBounds(1010,270,1240,275);
-        h19.setBounds(1240,270,1380,275);
-        h20.setBounds(440,320,580,325);
-        h21.setBounds(0,520,440,525);
-
-
-        v1.setBounds(0,0,5,1440);
-        v2.setBounds(2600,0,2605,1440);
-        v3.setBounds(2170,500,2175,850);
-        v4.setBounds(2170,850,2175,1080);
-        v5.setBounds(2170,1080,2175,1440);
-        v6.setBounds(1820,130,1825,695);
-        v7.setBounds(1830,920,1835,1440);
-        v8.setBounds(1600,920,1605,1440);
-        v9.setBounds(1260,920,1265,1440);
-        v10.setBounds(920,920,925,1440);
-        v11.setBounds(440,130,445,690);
-        v12.setBounds(580,270,585,690);
-        v13.setBounds(1010,270,1015,690);
-        v14.setBounds(1240,270,1245,690);
-        v15.setBounds(1380,130,1385,690);
-        v16.setBounds(1950,0,1955,500);
-
-
-        walls.add(h1);
-        walls.add(h2);
-        walls.add(h3);
-        walls.add(h4);
-        //walls.add(h5);
-        //walls.add(h6);
-        walls.add(h7);
-        walls.add(h8);
-        //walls.add(h9);
-        walls.add(h10);
-        //walls.add(h11);
-        walls.add(h12);
-        walls.add(h13);
-        walls.add(h14);
-        walls.add(h15);
-        walls.add(h16);
-        walls.add(h17);
-        walls.add(h18);
-        walls.add(h19);
-        walls.add(h20);
-        walls.add(h21);
-
-
-
-        walls.add(v1);
-        walls.add(v2);
-        walls.add(v3);
-        //walls.add(v4);
-        walls.add(v5);
-        walls.add(v6);
-        walls.add(v7);
-        walls.add(v8);
-        walls.add(v9);
-        walls.add(v10);
-        walls.add(v11);
-        walls.add(v12);
-        walls.add(v13);
-        walls.add(v14);
-        walls.add(v15);
-        walls.add(v16);
-
+        for(int[] wallBound : wallsBounds) {
+            ShapeDrawable shape = new ShapeDrawable(new RectShape());
+            shape.setBounds(wallBound[0],wallBound[1],wallBound[2],wallBound[3]);
+            walls.add(shape);
+        }
 
         float scale = (float) width/2700;
 
@@ -290,8 +188,8 @@ public class MainActivity extends Activity implements OnClickListener {
 
 
         for(int i = 0; i < this.Particles.size(); i++) {
-            int x = this.Particles.get(i).get(0);
-            int y = this.Particles.get(i).get(1);
+            int x = this.Particles.get(i)[0];
+            int y = this.Particles.get(i)[1];
             // create a drawable object
             drawable = new ShapeDrawable(new OvalShape());
             drawable.getPaint().setColor(Color.BLUE);
@@ -304,7 +202,7 @@ public class MainActivity extends Activity implements OnClickListener {
 
     public void setParticles(){
 
-        this.Particles = new ArrayList<ArrayList<Integer>>();
+        this.Particles = new ArrayList<int[]>();
 
         int boundary = 10;
 
@@ -312,15 +210,15 @@ public class MainActivity extends Activity implements OnClickListener {
             for (int particles = 0; particles < this.RoomParticles.get(room).get(1); particles++){
 
                 int x1, x2, y1, y2, Px, Py;
-                x1 = this.RoomParticles.get(room).get(2) + boundary;
                 y1 = this.RoomParticles.get(room).get(3) + boundary;
+                x1 = this.RoomParticles.get(room).get(2) + boundary;
                 x2 = this.RoomParticles.get(room).get(4) - boundary;
                 y2 = this.RoomParticles.get(room).get(5) - boundary;
 
                 Px = ThreadLocalRandom.current().nextInt(x1, x2+1);
                 Py = ThreadLocalRandom.current().nextInt(y1, y2+1);
 
-                this.Particles.add(new ArrayList<Integer>(Arrays.asList(Px, Py)));
+                this.Particles.add(new int[] {Px, Py, 0});
             }
 
             if (this.RoomParticles.get(room).size() == 11) {
@@ -334,7 +232,7 @@ public class MainActivity extends Activity implements OnClickListener {
                     Px = ThreadLocalRandom.current().nextInt(x1, x2 + 1);
                     Py = ThreadLocalRandom.current().nextInt(y1, y2 + 1);
 
-                    this.Particles.add(new ArrayList<Integer>(Arrays.asList(Px, Py)));
+                    this.Particles.add(new int[] {Px, Py, 0});
                 }
             }
         }
@@ -374,39 +272,34 @@ public class MainActivity extends Activity implements OnClickListener {
             double changeX = new Double(distance * Math.sin(direction));
             double changeY = new Double(distance * Math.cos(direction));
 
-            //int iterationsX = (int) changeX/movementSize;
-            //int iterationsY = (int) changeY/movementSize;
-            //int iterations = Math.max(iterationsX, iterationsY);
-
-
             while (changeX != 0 && changeY != 0) {
 
                 if (changeX >= 3) {
-                    int newX = this.Particles.get(p).get(0) + movementSize;
-                    this.Particles.get(p).set(0, newX);
+                    int newX = this.Particles.get(p)[0] + movementSize;
+                    this.Particles.get(p)[0] = newX;
                     changeX = changeX - movementSize;
                 } else {
-                    int newX = this.Particles.get(p).get(0) + (int) changeX;
-                    this.Particles.get(p).set(0, newX);
+                    int newX = this.Particles.get(p)[0] + (int) changeX;
+                    this.Particles.get(p)[0] = newX;
                     changeX = 0;
                 }
 
                 if (changeY >= 3) {
-                    int newY = this.Particles.get(p).get(0) + movementSize;
-                    this.Particles.get(p).set(0, newY);
+                    int newY = this.Particles.get(p)[0]+ movementSize;
+                    this.Particles.get(p)[0] = newY;
                     changeY = changeY - movementSize;
                 } else {
-                    int newY = this.Particles.get(p).get(0) + (int) changeY;
-                    this.Particles.get(p).set(0, newY);
+                    int newY = this.Particles.get(p)[0] + (int) changeY;
+                    this.Particles.get(p)[0] = newY;
                     changeY = 0;
                 }
 
                 drawable = new ShapeDrawable(new OvalShape());
 
-                drawable.setBounds(this.Particles.get(p).get(0) - this.radiusParticles,
-                        this.Particles.get(p).get(1) - radiusParticles,
-                        this.Particles.get(p).get(0) + this.radiusParticles,
-                        this.Particles.get(p).get(1) + radiusParticles);
+                drawable.setBounds(this.Particles.get(p)[0] - this.radiusParticles,
+                        this.Particles.get(p)[1] - radiusParticles,
+                        this.Particles.get(p)[0] + this.radiusParticles,
+                        this.Particles.get(p)[1] + radiusParticles);
 
                 // if there is a collision between the dot and any of the walls
                 if(isCollision()) {
@@ -421,25 +314,6 @@ public class MainActivity extends Activity implements OnClickListener {
 
 
     }
-
-
-
-        /*
-        for (int p = 0; p < this.Particles.size(); p++){
-
-            double tempX = new Double(distance * Math.sin(direction));
-
-            int newX = this.Particles.get(p).get(0) + (int) tempX;
-            this.Particles.get(p).set(0, newX);
-
-            double tempY = new Double(distance * Math.cos(direction));
-
-            int newY = this.Particles.get(p).get(1) + (int) tempY;
-            this.Particles.get(p).set(1, newY);
-
-        }
-        */
-
 
 
     @Override
@@ -528,10 +402,10 @@ public class MainActivity extends Activity implements OnClickListener {
     private  int findParticle(int nr, int room) {
         int count = 0;
         for(int i = 0; i < this.Particles.size(); i++) {
-            if (this.Particles.get(i).size() == 2) {
+            if (this.Particles.get(i).length == 2) {
                 continue;
             }
-            if (this.Particles.get(i).get(2) == room) {
+            if (this.Particles.get(i)[2] == room) {
                 if (nr == count) {
                     break;
                 } else {
@@ -557,9 +431,8 @@ public class MainActivity extends Activity implements OnClickListener {
         for (int idx : this.collidedParticles) {
             int particleNr = ThreadLocalRandom.current().nextInt(0, maxValue);
             int particleIdx = this.findParticle(particleNr, this.propabilityRoom.get(currentKey));
-            List<Integer> particle = this.Particles.get(particleIdx);
-            ArrayList<Integer> newParticle = new ArrayList<Integer>(Arrays.asList(particle.get(0), particle.get(1), particle.get(2)));
-            this.Particles.set(idx, newParticle);
+            int[] particle = this.Particles.get(particleIdx);
+            this.Particles.set(idx, particle.clone());
         }
         this.collidedParticles.clear();
 
@@ -571,8 +444,8 @@ public class MainActivity extends Activity implements OnClickListener {
 
 
         for (int particle = 0; particle < this.Particles.size(); particle++){
-            int Px = this.Particles.get(particle).get(0);
-            int Py = this.Particles.get(particle).get(1);
+            int Px = this.Particles.get(particle)[0];
+            int Py = this.Particles.get(particle)[1];
 
             if (this.collidedParticles.contains(particle)){
                 continue;
@@ -592,12 +465,9 @@ public class MainActivity extends Activity implements OnClickListener {
                     else {
                         this.propabilityRoom.put(roomNr, 1);
                     }
-                    if (this.Particles.get(particle).size() == 2){
-                        this.Particles.get(particle).add(roomNr);
-                    }
-                    else {
-                        this.Particles.get(particle).set(2, roomNr);
-                    }
+
+                    // set room
+                    this.Particles.get(particle)[2] = roomNr;
                 }
 
                 if (this.RoomParticles.get(room).size() == 11){
@@ -614,12 +484,8 @@ public class MainActivity extends Activity implements OnClickListener {
                         else {
                             this.propabilityRoom.put(roomNr, 1);
                         }
-                        if (this.Particles.get(particle).size() == 2){
-                            this.Particles.get(particle).add(roomNr);
-                        }
-                        else {
-                            this.Particles.get(particle).set(2, roomNr);
-                        }
+                        // set room
+                        this.Particles.get(particle)[2] = roomNr;
                     }
 
                 }
